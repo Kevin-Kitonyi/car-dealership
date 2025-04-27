e1a43eba96ab67d0eab7a5dc0c73165e23f2e86f
Kevin Kitonyi <kevinkitonyi8@gmail.com>
2025-04-27 14:42:38 +0300

    Removing obselete features

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -104,7 +104,7 @@
 
 			<div class="container">
 				<div class="welcome-hero-txt">
-					<h2>get your desired car in resonable price</h2>
+					<h2>get your desired car for a resonable price</h2>
 					<p>
 						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore   magna aliqua. 
 					</p>
@@ -202,9 +202,6 @@
 									<div class="col-md-5 col-sm-12">
 										<div class="new-cars-txt">
 											<h2><a href="#">chevrolet camaro <span> za100</span></a></h2>
-											<p>
-												Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-											</p>
 											<button class="welcome-btn new-cars-btn" onclick="window.location.href='#'">
 												view details
 											</button>
@@ -224,9 +221,6 @@
 									<div class="col-md-5 col-sm-12">
 										<div class="new-cars-txt">
 											<h2><a href="#">BMW series-3 wagon</a></h2>
-											<p>
-												Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-											</p>
 											<button class="welcome-btn new-cars-btn" onclick="window.location.href='#'">
 												view details
 											</button>
@@ -260,18 +254,14 @@
 									<div class="featured-model-info">
 										<p>
 											model: 2017
-											<span class="featured-mi-span"> 3100 mi</span> 
-											<span class="featured-hp-span"> 240HP</span>
-											 automatic
+											<span class="featured-mileage-span">Mileage: 90,000km</span> 
+											<span class="featured-color-span">Color: Black</span>
+											 Transmission: automatic
 										</p>
 									</div>
 								</div>
 								<div class="featured-cars-txt">
 									<h2><a href="#">BMW 6-series gran coupe</a></h2>
-									<h3>$89,395</h3>
-									<p>
-										Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non. 
-									</p>
 								</div>
 							</div>
 						</div>
@@ -284,18 +274,14 @@
 									<div class="featured-model-info">
 										<p>
 											model: 2017
-											<span class="featured-mi-span"> 3100 mi</span> 
-											<span class="featured-hp-span"> 240HP</span>
-											 automatic
+											<span class="featured-mileage-span">Mileage: 50,000km</span> 
+											<span class="featured-color-span">Color: Red</span>
+											 Transmission: automatic
 										</p>
 									</div>
 								</div>
 								<div class="featured-cars-txt">
 									<h2><a href="#">chevrolet camaro <span>wmv20</span></a></h2>
-									<h3>$66,575</h3>
-									<p>
-										Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non. 
-									</p>
 								</div>
 							</div>
 						</div>
@@ -308,18 +294,14 @@
 									<div class="featured-model-info">
 										<p>
 											model: 2017
-											<span class="featured-mi-span"> 3100 mi</span> 
-											<span class="featured-hp-span"> 240HP</span>
-											 automatic
+											<span class="featured-mileage-span">Mileage: 11,000km</span> 
+											<span class="featured-color-span">Color: Blue</span>
+											 Transmission: automatic
 										</p>
 									</div>
 								</div>
 								<div class="featured-cars-txt">
 									<h2><a href="#">lamborghini <span>v520</span></a></h2>
-									<h3>$125,250</h3>
-									<p>
-										Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non. 
-									</p>
 								</div>
 							</div>
 						</div>
@@ -327,121 +309,19 @@
 							<div class="single-featured-cars">
 								<div class="featured-img-box">
 									<div class="featured-cars-img">
-										<img src="assets/images/featured-cars/fc4.png" alt="cars">
+										<img src="assets/images/featured-cars/fc3.png" alt="cars">
 									</div>
 									<div class="featured-model-info">
 										<p>
 											model: 2017
-											<span class="featured-mi-span"> 3100 mi</span> 
-											<span class="featured-hp-span"> 240HP</span>
-											 automatic
+											<span class="featured-mileage-span">Mileage: 11,000km</span> 
+											<span class="featured-color-span">Color: Blue</span>
+											 Transmission: automatic
 										</p>
 									</div>
 								</div>
 								<div class="featured-cars-txt">
-									<h2><a href="#">audi <span> a3</span> sedan</a></h2>
-									<h3>$95,500</h3>
-									<p>
-										Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non. 
-									</p>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-lg-3 col-md-4 col-sm-6">
-							<div class="single-featured-cars">
-								<div class="featured-img-box">
-									<div class="featured-cars-img">
-										<img src="assets/images/featured-cars/fc4.png" alt="cars">
-									</div>
-									<div class="featured-model-info">
-										<p>
-											model: 2017
-											<span class="featured-mi-span"> 3100 mi</span> 
-											<span class="featured-hp-span"> 240HP</span>
-											 automatic
-										</p>
-									</div>
-								</div>
-								<div class="featured-cars-txt">
-									<h2><a href="#">infiniti <span>z5</span></a></h2>
-									<h3>$36,850</h3>
-									<p>
-										Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non. 
-									</p>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-3 col-md-4 col-sm-6">
-							<div class="single-featured-cars">
-								<div class="featured-img-box">
-									<div class="featured-cars-img">
-										<img src="assets/images/featured-cars/fc5.png" alt="cars">
-									</div>
-									<div class="featured-model-info">
-										<p>
-											model: 2017
-											<span class="featured-mi-span"> 3100 mi</span> 
-											<span class="featured-hp-span"> 240HP</span>
-											 automatic
-										</p>
-									</div>
-								</div>
-								<div class="featured-cars-txt">
-									<h2><a href="#">porsche <span>718</span> cayman</a></h2>
-									<h3>$48,500</h3>
-									<p>
-										Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non. 
-									</p>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-3 col-md-4 col-sm-6">
-							<div class="single-featured-cars">
-								<div class="featured-img-box">
-									<div class="featured-cars-img">
-										<img src="assets/images/featured-cars/fc7.png" alt="cars">
-									</div>
-									<div class="featured-model-info">
-										<p>
-											model: 2017
-											<span class="featured-mi-span"> 3100 mi</span> 
-											<span class="featured-hp-span"> 240HP</span>
-											 automatic
-										</p>
-									</div>
-								</div>
-								<div class="featured-cars-txt">
-									<h2><a href="#"><span>bmw 8-</span>series coupe</a></h2>
-									<h3>$56,000</h3>
-									<p>
-										Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non. 
-									</p>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-3 col-md-4 col-sm-6">
-							<div class="single-featured-cars">
-								<div class="featured-img-box">
-									<div class="featured-cars-img">
-										<img src="assets/images/featured-cars/fc8.png" alt="cars">
-									</div>
-									<div class="featured-model-info">
-										<p>
-											model: 2017
-											<span class="featured-mi-span"> 3100 mi</span> 
-											<span class="featured-hp-span"> 240HP</span>
-											 automatic
-										</p>
-									</div>
-								</div>
-								<div class="featured-cars-txt">
-									<h2><a href="#">BMW <span> x</span>series-6</a></h2>
-									<h3>$75,800</h3>
-									<p>
-										Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non. 
-									</p>
+									<h2><a href="#">lamborghini <span>v520</span></a></h2>
 								</div>
 							</div>
 						</div>
@@ -657,8 +537,7 @@
 					<div class="row">
 						<div class="col-sm-6">
 							<p>
-								&copy; copyright.designed and developed by <a href="https://www.themesine.com/">themesine</a>.
-							</p><!--/p-->
+								&copy; copyright.designed and developed by Kevin K</p><!--/p-->
 						</div>
 						<div class="col-sm-6">
 							<div class="footer-social">
